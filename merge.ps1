cd5534818d831fe363524de3566fc75f9f348c80
Kane Mason <kanemason@me.com>
2021-04-09 12:24:47 +0200

    delete shell scripts, just causes confusion now

--- merge.ps1
+++
@@ -1,7 +0,0 @@
-
-# PowerShell script to merge the several src/*.ts files into file src/tcx.ts
-# Chris Joakim, 2019/07/29
-
-node dist/merge.js src/tcx.ts
-
-echo 'done'
--- merge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Bash script to merge the several src/*.ts files into file src/tcx.ts
-# Chris Joakim, 2019/07/26
-
-node dist/merge.js src/tcx.ts
-
-echo 'done'
